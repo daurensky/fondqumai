a7dcf0b0314c5eca8cdf724ab6f737a4c1710b79
daurensky <dkambarov17@gmail.com>
2023-01-16 05:10:03 +0600

    initialize with index page (only mobile)

---
+++ _config.ts
@@ -0,0 +1,38 @@
+import lume from 'lume/mod.ts'
+import multilanguage from 'lume/plugins/multilanguage.ts'
+import postcss from 'lume/plugins/postcss.ts'
+import pug from 'lume/plugins/pug.ts'
+import tailwindcss from 'lume/plugins/tailwindcss.ts'
+
+const site = lume({
+  src: './src',
+})
+
+site.use(
+  tailwindcss({
+    options: {
+      theme: {
+        extend: {
+          colors: {
+            primary: {
+              blue: '#008dd2',
+            },
+          },
+          fontFamily: {
+            rubik: ['Rubik'],
+          },
+        },
+      },
+    },
+  })
+)
+
+site.use(postcss())
+
+site.use(pug())
+
+site.use(multilanguage())
+
+site.copy('assets/img')
+
+export default site
